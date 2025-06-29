1f2e06e37e33f0c89226c8f873ae3b175e61eb81
index <inderjotsingh141@gmail.com>
2025-06-29 09:38:30 +0530

    prevent create tourney without auth

--- smashkarts-ui/src/app/tournament/create/page.tsx
+++ smashkarts-ui/src/app/tournament/create/page.tsx
@@ -1,6 +1,13 @@
+import { redirect } from "next/navigation";
 import CreateTournamentForm from "./form";
+import { getServerSession } from "@/auth/auth-server";
 
-export default function CreateTournamentPage() {
+export default async function CreateTournamentPage() {
+  const session = await getServerSession();
+  if (!session?.user?.id) {
+    redirect("/login");
+  }
+
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <CreateTournamentForm />
